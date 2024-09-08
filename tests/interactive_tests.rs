988c84e6975fc5d4d316da4dfb4699174bd2b19d
Sebastian Hoß <seb@xn--ho-hia.de>
2024-09-08 09:07:06 +0200

    test w/ ssh rsa key
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- tests/interactive_tests.rs
+++ tests/interactive_tests.rs
@@ -5,40 +5,42 @@ use assert_fs::TempDir;
 use predicates::prelude::*;
 use rexpect::session::PtySession;
 use rexpect::spawn;
+use serial_test::serial;
 use ssh_key::rand_core::OsRng;
 use ssh_key::{Algorithm, PrivateKey};
 use std::env;
 use std::process::Command;
 
 #[test]
+#[serial]
 fn insert_secret_with_age_key() -> anyhow::Result<()> {
-    let secret_name = "some/secret";
     let key = age::x25519::Identity::generate();
-    run_command(
-        &format!("secret insert {secret_name}"),
-        &key.to_public().to_string(),
-        |mut process, temp| {
-            process.exp_string(&format!("Enter secret for {secret_name}:"))?;
-            process.send_line("some-secret")?;
-            process.exp_string("Confirmation:")?;
-            process.send_line("some-secret")?;
-            process.exp_eof()?;
-
-            temp.child(format!("{secret_name}.age"))
-                .assert(predicate::path::is_file());
-            Ok(())
-        },
-    )?;
+    insert_secret_with_key(&key.to_public().to_string())?;
     Ok(())
 }
 
 #[test]
-fn insert_secret_with_ssh_key() -> anyhow::Result<()> {
-    let secret_name = "some/secret";
+#[serial]
+fn insert_secret_with_ssh_ed25519_key() -> anyhow::Result<()> {
     let key = PrivateKey::random(&mut OsRng::default(), Algorithm::Ed25519)?;
+    insert_secret_with_key(&key.public_key().to_string())?;
+    Ok(())
+}
+
+#[test]
+#[serial]
+#[ignore] // it takes too long to run - execute with: cargo test -- --ignored
+fn insert_secret_with_ssh_rsa_key() -> anyhow::Result<()> {
+    let key = PrivateKey::random(&mut OsRng::default(), Algorithm::Rsa { hash: None })?;
+    insert_secret_with_key(&key.public_key().to_string())?;
+    Ok(())
+}
+
+fn insert_secret_with_key(public_key: &str) -> anyhow::Result<()> {
+    let secret_name = "some/secret";
     run_command(
         &format!("secret insert {secret_name}"),
-        &key.public_key().to_string(),
+        public_key,
         |mut process, temp| {
             process.exp_string(&format!("Enter secret for {secret_name}:"))?;
             process.send_line("some-secret")?;
