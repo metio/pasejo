de292076f51892d530b66c85c0748a603c0cf971
Sebastian Hoß <seb@xn--ho-hia.de>
2025-05-21 17:06:40 +0200

    add missing license header
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: The pasejo Authors
+// SPDX-License-Identifier: 0BSD
+
 use crate::models::configuration::{Configuration, ConfigurationOption};
 
 pub fn get(configuration: &Configuration, option: &ConfigurationOption) {
