fa27cdc377fb3ddf3a94049705e68a3299b38d4c
Sebastian Hoß <seb@xn--ho-hia.de>
2026-05-04 20:09:42 +0200

    validate usernames
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- src/downloader/gitlab.rs
+++ src/downloader/gitlab.rs
@@ -1,15 +1,18 @@
 // SPDX-FileCopyrightText: The pasejo Authors
 // SPDX-License-Identifier: 0BSD
 
+use crate::cli::environment_variables;
+use crate::downloader::username;
+use anyhow::Context;
 use std::env;
 
-use crate::cli::environment_variables;
-
 pub fn download_public_key(username: &str) -> anyhow::Result<String> {
+    let username = username::validate(username)?;
     let gitlab_host =
         env::var(environment_variables::GITLAB_HOST).unwrap_or_else(|_| String::from("gitlab.com"));
     let key = ureq::get(format!("https://{gitlab_host}/{username}.keys"))
-        .call()?
+        .call()
+        .context("Downloading public key from gitlab failed")?
         .body_mut()
         .read_to_string()?;
     Ok(String::from(key.trim()))
--- tests/cmd/recipient/add/add-from-github.md
+++ tests/cmd/recipient/add/add-from-github.md
@@ -6,4 +6,8 @@ $ pasejo recipient add --github sebhoss
 Recipient for 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOycTzXsl1jetsf+Ezi/2FCVU8oydXxxJhb9D7n6MlGf' added
 There are no identities in the store 'something' yet. Please add one using 'pasejo identity add ...'
 
+$ pasejo recipient add --github seb/hoss
+? 1
+Error: Invalid username 'seb/hoss': must start with an ASCII letter or digit and contain only ASCII letters, digits, '-', '_' and '.'
+
 ```
