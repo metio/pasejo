d4aafddfa87d2dcb84aece42f611fab766a9c337
Sebastian Hoß <seb@xn--ho-hia.de>
2026-05-08 15:53:35 +0200

    remove const-str
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- src/cli/environment_variables.rs
+++ src/cli/environment_variables.rs
@@ -1,27 +1,10 @@
 // SPDX-FileCopyrightText: The pasejo Authors
 // SPDX-License-Identifier: 0BSD
 
-use crate::cli::constants;
-
-pub const PASEJO_CONFIG: &str = const_str::convert_ascii_case!(
-    upper,
-    const_str::concat!(constants::APPLICATION_NAME, "_config")
-);
-
-pub const PASEJO_DEFAULT_STORE: &str = const_str::convert_ascii_case!(
-    upper,
-    const_str::concat!(constants::APPLICATION_NAME, "_default_store")
-);
-
-pub const PASEJO_DATA_DIR: &str = const_str::convert_ascii_case!(
-    upper,
-    const_str::concat!(constants::APPLICATION_NAME, "_data_dir")
-);
-
-pub const PASEJO_DISABLE_HOOK_THROTTLING: &str = const_str::convert_ascii_case!(
-    upper,
-    const_str::concat!(constants::APPLICATION_NAME, "_disable_hook_throttling")
-);
+pub const PASEJO_CONFIG: &str = "PASEJO_CONFIG";
+pub const PASEJO_DEFAULT_STORE: &str = "PASEJO_DEFAULT_STORE";
+pub const PASEJO_DATA_DIR: &str = "PASEJO_DATA_DIR";
+pub const PASEJO_DISABLE_HOOK_THROTTLING: &str = "PASEJO_DISABLE_HOOK_THROTTLING";
 
 pub const CODEBERG_HOST: &str = "CODEBERG_HOST";
 pub const GITHUB_HOST: &str = "GITHUB_HOST";
