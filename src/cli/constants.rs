d1af9547bc2b87cc0e4d8e1a56d8578b1b26ea04
Sebastian Hoß <seb@xn--ho-hia.de>
2026-05-17 20:27:31 +0200

    extract constants
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- src/cli/constants.rs
+++ src/cli/constants.rs
@@ -2,3 +2,18 @@
 // SPDX-License-Identifier: 0BSD
 
 pub const APPLICATION_NAME: &str = env!("CARGO_PKG_NAME");
+
+pub const DEFAULT_USERNAME_KEYS: &[&str] = &["login", "email", "username"];
+
+pub const DEFAULT_URI_KEYS: &[&str] = &["uri", "url", "link", "site"];
+
+pub fn default_username_keys() -> Vec<String> {
+    DEFAULT_USERNAME_KEYS
+        .iter()
+        .map(|s| (*s).to_owned())
+        .collect()
+}
+
+pub fn default_uri_keys() -> Vec<String> {
+    DEFAULT_URI_KEYS.iter().map(|s| (*s).to_owned()).collect()
+}
